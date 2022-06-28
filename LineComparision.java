3be45c2c6d98080bef6b35cd7a685087eb5f4df4
Syerne <shubhamyerne1994@gmail.com>
2022-06-28 16:23:01 +0530

    Added : Check Equality, largest , smallest line

--- LineComparision.java
+++ LineComparision.java
@@ -1,6 +1,45 @@
-public class LineComparision {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Line Comparison Problem");
+import java.util.Scanner;
 
+public class LineComparision {
+
+    public static double lineLength() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter x1 point");
+        int x1 = sc.nextInt();
+        System.out.println("Enter y1 point");
+        int y1 = sc.nextInt();
+        System.out.println("Enter x2 point");
+        int x2 = sc.nextInt();
+        System.out.println("Enter y2 point");
+        int y2 = sc.nextInt();
+
+        int X = x2 - x1;
+        int Y = y2 - y1;
+        double length = Math.sqrt((X * X) + (Y * Y));
+        return length;
+    }
+
+    public static void Comparison(double length1, double length2) {
+        if (length1 == length2) {
+            System.out.println("Line1 is equal to Line2");
+        } else if (length1 > length2) {
+            System.out.println("line1 is greater than line2");
+
+        } else {
+            System.out.println("Line2 is greater than Line1");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Program");
+
+        System.out.println("Enter coordinate of line-1");
+        double length1 = lineLength();
+        System.out.println("Enter coordinate of line-2");
+        double length2 = lineLength();
+        System.out.println("length of line-1 :" + length1 + "\n" + "lengthof line-2 :" + length2);
+        Comparison(length1, length2);
     }
 }
